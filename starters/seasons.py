a0f420cac5634162f1027fbc82b07ab6ad2d9090
guy-glitch <warrenjgibson@gmial.com>
2025-08-21 09:05:24 -0600

    Updates and adding the seasons starter

---
+++ starters/seasons.py
@@ -0,0 +1,6 @@
+#WG Seasons
+#asks for last name and favorite season
+lastname = input("What is Your Name?")
+season = input("What is your Favorite Season?")
+
+print(f"Wow, {lastname} san, I like {season} too!")
