abcc30cfa7533e7651294aea34e77c26c941ce37
guy-glitch <warrenjgibson@gmial.com>
2025-08-22 09:53:00 -0600

    changes to an unfished hello world and new unfished who are you project

--- Practice/Hello_World.py
+++ Practice/Hello_World.py
@@ -2,10 +2,21 @@
 #Prints login page
 print("Please Login")
 #Greets users by name and if they are returning user
-returning = ("Dirk, Miari, Bryor, Brecken, Milo")
 admin = ("Warren, Ms. LaRose")
+returning = ("Dirk, Miari, Bryor, Brecken, Milo")
+name = input("What is your name")
+user = (f"{admin}, {returning} + {name}")
+password_new = input("enter your password")
+password_larose = ("programming")
+password_warren = ("skills")
+if name == "Ms. LaRose":
+    if password_new == password_larose:
+        name = name 
+        print(f"Hello Admin {name}")
+if name == "Warren":
+    if password_new == password_warren:
+        name = name.capitalize()
 # asks for name and if it is an admin does not auto capitilize other wise it capitilzes
-name = input("what is your name")
 if name == "Ms. LaRose":
     name = name
 else:
---
+++ Projects/who_are_you.py
@@ -0,0 +1,9 @@
+run = input("Would you like to run y/n")
+while run == "y":
+    name = input("What is your name")
+    age =  input("How old are you")
+    color = input("What is your favorite color")
+    print = (f"So your name is {name}, you are {age} years old, and your favorite color is {color}")
+    run = input("Would you like to run again y/n")
+if run == "n":
+    raise SystemExit("Exiting program.")
