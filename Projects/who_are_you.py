2c428109f5b41eb787fe37367aa91f50a5f27c07
guy-glitch <warrenjgibson@gmial.com>
2025-08-25 09:00:48 -0600

    finished the who are you project did the while loop but coudl'nt figure out the dictionaries nessacary to do the rest

--- Projects/who_are_you.py
+++ Projects/who_are_you.py
@@ -1,9 +1,12 @@
+import sys
 run = input("Would you like to run y/n")
 while run == "y":
     name = input("What is your name")
     age =  input("How old are you")
     color = input("What is your favorite color")
-    print = (f"So your name is {name}, you are {age} years old, and your favorite color is {color}")
+    print(f"So your name is {name}, you are {age} years old, and your favorite color is {color}")
     run = input("Would you like to run again y/n")
 if run == "n":
-    raise SystemExit("Exiting program.")
+    print("Terminating code")
+    sys.exit
+
