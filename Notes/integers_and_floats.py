9cecd4c53c2275252879d4f43636d968284f72c1
guy-glitch <warrenjgibson@gmial.com>
2025-08-27 09:52:48 -0600

    begining of notes

--- Notes/integers_and_floats.py
+++ Notes/integers_and_floats.py
@@ -1,1 +1,38 @@
 #WG_1st_integers_floats_notes
+year = 2015
+room = 200
+hour = 9
+motivation = -5
+
+#Float ("Floating point number")
+pi = 3.14
+percent = -.75
+damage = 9.75
+
+#MATH
+print(5+2)#7
+print(5-2)#3
+print(5*2)#10
+print(5/2)#2.5
+print(5//2)#2
+print(5%2)#1
+print(5**2)#25
+
+print(5+2 *9+ (5**5)%3)#25
+#type casting
+num = int(3.14)#does not round 
+decimal = float(8)
+print(num)
+print(decimal)
+#useing Variable
+hours = 3
+minutes = 4
+
+print(hours*60+minutes)
+
+
+
+age = int(input("How old are you"))
+
+print("Wow you are old", 525600*age, "minustes!")
+
