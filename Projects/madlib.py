2d67913fb2a938354613badb21c249e2c3d256eb
guy-glitch <warrenjgib@gmail.com>
2025-09-03 09:53:24 -0600

    madlib project

---
+++ Projects/madlib.py
@@ -0,0 +1,35 @@
+#WG_1st_MadLib
+#Asks for needed information
+adjective_one = input("Give me an Adjective")
+
+adjective_two = input("Give me another Adjective")
+
+number = input("Give me a number noun")
+
+verb_ing = input("Give me a verb ending in ing")
+
+school_room = input("Give the name of a room at school")
+
+name = input("Give me a first name")
+
+age = input("give me a number")
+
+verb = input("Give me a verb")
+
+color = input("Give me a color")
+
+print("Adverbs are words that modify a adjective")
+adverb = input("Give me an Adverb")
+
+adjective_three = input("Give me another adjective")
+
+verb_two = input("Give me a verb")
+
+print("This might sound "+adjective_one+", but my "+adjective_two+" friend is a ghost. We met in "+number+" grade at school while they were "+verb_ing+" the "+school_room+". Their name is "+name+" and they were "+number+" years old when they died. No one can "+verb+" my friend but me. They have "+color+" hair are are "+adverb+" "+adjective_two+", and their clothes are "+adjective_three+". Having an invisible friend is "+adjective_one+", but they always win at "+verb+" and seek.")
+
+
+
+
+
+
+
