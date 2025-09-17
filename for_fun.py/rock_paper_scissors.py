be51eb791489d06eb116884a465cb7c7bc91ca9c
guy-glitch <warrenjgib@gmail.com>
2025-09-17 09:46:04 -0600

    Message

--- for_fun.py/rock_paper_scissors.py
+++ for_fun.py/rock_paper_scissors.py
@@ -1,12 +1,14 @@
 import random
 run = "Y"
 play = input("Do you want to play rock paper scissors? Yes or No").strip().lower()
-
+win = 0
+tie = 0
+lose = 0
 while run == "Y":
     if play == "no":
         print("See Ya")
     else:
-        com_choice = random.randomint(1,3)
+        com_choice = random.randint(1,3)
         player = input("Rock, Paper, Scissors, Shoot").strip().lower()
         if player == "rock":
             if com_choice == 1:
@@ -18,6 +20,8 @@ while run == "Y":
             if com_choice == 1:
                 print(f"Paper")
                 print("You lose")
+            else:
+                run = input("Do you want to play again.")
         if player == "Paper":
             if com_choice == 1:
                 print(f"Scissors")
@@ -28,4 +32,18 @@ while run == "Y":
             if com_choice == 1:
                 print(f"Paper")
                 print("We tie")
+            else:
+                run = input("Do you want to play again.")
+        if player == "Scissors":
+            if com_choice == 1:
+                print(f"Scissors")
+                print("We tie")
+            if com_choice == 1:
+                print(f"Rock")
+                print("You lose")
+            if com_choice == 1:
+                print(f"Paper")
+                print("You win")
+            else:
+                run = input("Do you want to play again.")
                 
