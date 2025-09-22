b96f5c8b8cec57abf747b0c17e3b0bff3fcbc3e6
guy-glitch <warrenjgib@gmail.com>
2025-09-22 09:28:43 -0600

    user_signin project

---
+++ Projects/user_signin.py
@@ -0,0 +1,16 @@
+#WG_1st_User_Sign_In
+number_of_logins = 4
+while number_of_logins > 0:
+    username = input("What is your username?").strip().lower()
+    if username == "warren.gibson@ucas-edu.net":
+        while number_of_logins > 0:
+            password = input("Enter your password!").strip().lower()
+            if password == "1443":
+                print("Welcome, Warren")
+                number_of_logins = 0
+            else:
+                number_of_logins -= 1
+                print(f"Incorrect password. You have {number_of_logins} login attempts left.")
+    else:
+        number_of_logins -= 1
+        print(f"Incorrect user name. You have {number_of_logins} logins attempts left.")
