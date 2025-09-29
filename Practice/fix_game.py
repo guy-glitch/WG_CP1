73fe2e9bf9ca136b6aa4a2282f3f4026aae79f04
guy-glitch <warrenjgib@gmail.com>
2025-09-29 08:55:52 -0600

    found all of the errors and named them

--- Practice/fix_game.py
+++ Practice/fix_game.py
@@ -1,4 +1,3 @@
-#WG 1st Fix Code
 import random
 def start_game():
     print("Welcome to the Number Guessing Game!")
@@ -8,25 +7,24 @@ def start_game():
     attempts = 0
     game_over = False
     while not game_over:
-        #This was a string that was trying to have math be done with it so I converted it to a float, added .strip to solve any spaces added before the answer is typed. The first error is a 
-        guess = float(input("Enter your guess: ").strip())
+        #converting it to a float so the greater then works this is a runtime error
+        guess = float(input("Enter your guess: "))
         if attempts >= max_attempts:
             print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
             game_over = True
-        #Changed to an elif so it worked correctly logical error
-        elif guess == number_to_guess:
+        if guess == number_to_guess:
             print("Congratulations! You've guessed the number!")
             game_over = True
         elif guess > number_to_guess:
             print("Too high! Try again.")
-            #Attempts were not changeing so the code repeated forever this is a logical error
+            #Increase the attempts taken by 1 so that it ends. This is a logical error.
             attempts += 1
         elif guess < number_to_guess:
-            print("Too low! Try again.")  
-            attempts += 1
-        #not compensating for invalid inputs added else statement this is a Runetime error
+            print("Too low! Try again.")
+            attempts += 1  
+        #The continue was redundant This is a logical error.
+        #There wasn't an else to catch everything that failed. This is a syntax error 
         else:
-            print("Incorrect input, please enter a number")
-        continue
+            print("Incorrect input try again")
     print("Game Over. Thanks for playing!")
 start_game()
