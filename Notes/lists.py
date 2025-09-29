ae4d3e5405b35554da77e81549e2a61e3b8a67ef
guy-glitch <warrenjgib@gmail.com>
2025-09-29 09:49:26 -0600

    lists

---
+++ Notes/lists.py
@@ -0,0 +1,21 @@
+#WG 1st list notes
+sibs = ["Alexander", "Andrew", "Jacob", "Fred"]
+family = ["GR", "Father"]
+import random
+print(sibs[3])
+sibs[3] = "Jake"
+sibs[3:-1] = ["Xaveir", "Hailey"]
+print(sibs)
+sibs.pop()
+
+board = [[1,2,3],
+         [4,5,6],
+         [7,8,9]
+        ]
+print(board)
+fruit = ("Apple", "Orange", "Pineapple")#tuple ordered, not changeable
+
+veggies = {"Spinach", "Cucumber", "Carrot"}#set unordered, changeable
+print(veggies)
+
+
