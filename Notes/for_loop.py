c8c5262a128eb6e78e0eb7c50e011b87c2eaeb79
guy-glitch <warrenjgib@gmail.com>
2025-10-01 09:30:01 -0600

    f

--- Notes/for_loop.py
+++ Notes/for_loop.py
@@ -1,5 +1,14 @@
 #WG 1st For Loop Notes
-listname = ["Items", "In", "The", "List"]
+listname = [4,15,16,17,287,235,2,654,2542,526364]
 
 for num in listname:
+    num/=2
+    if num > 100:
+        print(f"{num}is only half of {num*2}. It is a large number")
+    else:
+        print(num)
+
+print("The fruit loop is over")
+
+for num in range(1,10):
     print(num)
