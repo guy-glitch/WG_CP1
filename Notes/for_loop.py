7fe1149bf99bad44d5d74d934bcb45b06d6f2d92
guy-glitch <warrenjgib@gmail.com>
2025-10-01 09:41:32 -0600

    just comments

--- Notes/for_loop.py
+++ Notes/for_loop.py
@@ -1,14 +1,18 @@
 #WG 1st For Loop Notes
-listname = [4,15,16,17,287,235,2,654,2542,526364]
+list = [""]
+import time
+for i in list:
+    #listname = [4,15,16,17,287,235,2,654,2542,526364]
 
-for num in listname:
-    num/=2
-    if num > 100:
-        print(f"{num}is only half of {num*2}. It is a large number")
-    else:
-        print(num)
+    #for num in listname:
+        #num/=2
+        #if num > 100:
+            #print(f"{num}is only half of {num*2}. It is a large number")
+        #else:
+            #print(num)
 
-print("The fruit loop is over")
+    #print("The fruit loop is over")
 
-for num in range(1,10):
-    print(num)
+    #for num in range(1,1000000000,2):
+        #print(num)
+    print("")
