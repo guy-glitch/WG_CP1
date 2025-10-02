87430c15e05c92070a2bad9dd8160fc936c78eb7
guy-glitch <warrenjgib@gmail.com>
2025-10-02 09:02:21 -0600

    basic multiplication table

--- Practice/multiplication_table.py
+++ Practice/multiplication_table.py
@@ -1,4 +1,6 @@
 #WG 1st Multiplication table
 nums = [1,2,3,4,5,6,7,8,9,10,11,12]
 for num in nums:
-    print(num + nums())
+    print(f"{num*1} {num*2} {num*3} {num*4} {num*5} {num*6} {num*7} {num*8} {num*9} {num*10} {num*11} {num*12}")
+
+print("This is a multiplication 12 by 12")
