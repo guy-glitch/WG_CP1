cabcf043e895764b52df3536f507be4c56e5dccd
guy-glitch <warrenjgib@gmail.com>
2025-10-10 09:51:07 -0600

    test stregth of password

---
+++ Projects/password_strength_checker.py
@@ -0,0 +1,11 @@
+#WG 1st password strength checker
+#ask for the users password
+#get length of password
+#characters in password = []
+#for length of password
+    #for length of password
+        #
+        #characters.append()
+    #score = 0
+    #if length >= 8 and 
+        #score + 1
