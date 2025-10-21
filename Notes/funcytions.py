690c07a33b2dc4c986438de49476012a92454031
guy-glitch <warrenjgib@gmail.com>
2025-10-21 15:46:49 -0600

    functions changes

---
+++ Notes/funcytions.py
@@ -0,0 +1,17 @@
+#WG 1st Functions Notes
+def add(x, y):
+    return x + y
+def initials(name):
+    names = name.split(" ")\
+    for name in names:
+        initial += name[0]
+
+
+    return initials
+total = add(5,9)
+print(f"{add(10,5)}")
+x = 0
+while x < add(3,13):
+    print("duck")
+    x+=1
+print("goose")
