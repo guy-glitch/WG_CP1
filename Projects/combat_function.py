051d09eac300631f937da3196db328c5d2e95f7c
guy-glitch <warrenjgib@gmail.com>
2025-10-27 09:08:32 -0600

    combat function

--- Projects/combat_function.py
+++ Projects/combat_function.py
@@ -1,180 +1,189 @@
 #WG 1st combat function
+import time
 import random
-print("Chose your class")
+class_choice = input("Choose your species lich, demon, bunny").strip().lower()
 player = "alive"
 monster_life = "alive"
-monster_health = 30
-monster_defense = 17
-monster_attack = "D20 + 2"
-monster_damage = "D10 + 3"
+monster = {"health":30, "defense":17, "attack":"D20 + 2", "damage":"D8 + 3"}
 turn =  0
-while player == "alive" and monster_life == "alive":
-    def player_turn():
-        while turn == "Player":
-            if class_choice == "sacraficial warrior":
-                action = input("What would you like to do? \n sacrificial strike\n enemy sacrafice\nburning health\n sword strike")
-                if action == "sacrificial strike":
-                    health_sacraficed = int(input(f"how much health do you want to sacrace?\n You have {health} health"))
-                    print("Roll to hit")
-                    hit = random.randint(1,20) + health_sacraficed
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        print("You hit roll for damage")
-                        damage = random.randint(1,6) + health_sacraficed
-                        print(f"You did {damage}")
-                        monster_health -= damage
-                        turn = "Monster"
-                    else:
-                        print("You missed")
-                        turn = "Monster"
-                elif action == "enemy sacrafice":
-                    print("Roll to hit")
-                    hit = random.randint(1,20) + health/20
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        print("You hit roll for damage")
-                        damage = random.randint(1,6) + health/40
-                        print(f"You did {damage}")
-                        monster_health -= damage
-                        health += damage
-                        turn = "Monster"
-                    else:
-                        print("You missed")
-                        turn = "Monster"
-                elif action == "burning health":
-                    burn_health == input("Do you want to burn half of your health to double damage? yes or no")
-                    print("Roll to hit")
-                    hit = random.randint( 1,20) + health/20
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        if burn_health == "yes":
-                            print("You hit roll for damage")
-                            damage = (random.randint(1,6) + health/40) * 2
-                            print(f"You did {damage}")
-                            monster_health -= damage
-                            health = health / 2
-                            turn = "Monster"
-                        else:
-                            print("You hit roll for damage")
-                            damage = random.randint(1,6) + health/40
-                            print(f"You did {damage}")
-                            monster_health -= damage
-                            turn = "Monster"
-                    else:
-                        print("YOU missed")
-                        turn = "Monster"
-                elif action == "sword strike":
-                    print("Roll to hit")
-                    hit = random.randint(1,20) + health/20
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        print("You hit roll for damage")
-                        damage = random.randint(1,6) + health/40
-                        print(f"You did {damage}")
-                        monster_health -= damage
-                        turn = "Monster"
-                    else:
-                        print("You missed")
-                        turn = "Monster"
-            elif class_choice == "blur knife":
-                action = input("What would you like to do? \n multistrike\nbluring strike\n dagger strike")
-                if action == "multistrike":
-                    print("Roll to hit")
-                    hit = random.randint(1,20) + health_sacraficed
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        print("You hit roll for damage on your first strike")
-                        damage = ((random.randint(1,8) + 1)*2)/3 
-                        print(f"You did {damage}")
-                        monster_health -= damage
-                        print("You hit roll for damage on your second strike")
-                        damage = ((random.randint(1,8) + 1)*2)/3 
-                        print(f"You did {damage}")
-                        monster_health -= damage
-                        turn = "Monster"
-                    else:
-                        print("You missed")
-                        turn = "Monster"
-                elif action == "enemy sacrafice":
-                    print("Roll to hit")
-                    hit = random.randint(1,20) + health/20
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        print("You hit roll for damage")
-                        damage = random.randint(1,6) + health/40
-                        print(f"You did {damage}")
-                        monster_health -= damage
-                        health += damage
-                        turn = "Monster"
-                    else:
-                        print("You missed")
-                        turn = "Monster"
-                elif action == "burning health":
-                    burn_health == input("Do you want to burn half of your health to double damage? yes or no")
-                    print("Roll to hit")
-                    hit = random.randint( 1,20) + health/20
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        if burn_health == "yes":
-                            print("You hit roll for damage")
-                            damage = (random.randint(1,6) + health/40) * 2
-                            print(f"You did {damage}")
-                            monster_health -= damage
-                            health = health / 2
-                            turn = "Monster"
-                        else:
-                            print("You hit roll for damage")
-                            damage = random.randint(1,6) + health/40
-                            print(f"You did {damage}")
-                            monster_health -= damage
-                            turn = "Monster"
-                    else:
-                        print("YOU missed")
-                        turn = "Monster"
-                elif action == "sword strike":
-                    print("Roll to hit")
-                    hit = random.randint(1,20) + health/20
-                    print(f"{hit} was your roll to hit")
-                    if hit > monster_defense:
-                        print("You hit roll for damage")
-                        damage = random.randint(1,6) + health/40
-                        print(f"You did {damage}")
-                        monster_health -= damage
-                        turn = "Monster"
-                    else:
-                        print("You missed")
-                        turn = "Monster"
-            
-    def combat():
-        first = random.randint(1,2)
-        if first == 1:
-            turn = "Player"
-            print("You move first")
-            player_turn()
+stats = {"health": 30, "defense":14, "attack": "D20 + 2", "damage":"D10","attack one" : 0, "attack two":0}
+if class_choice == "bunny":
+    stats["health"] = 30
+    stats["defense"] = 14
+    stats["attack"] = "D20 + 2"
+    stats["damage"] = "D10"
+    stats["attack one"] = "multikick"
+    stats["attack two"] = "bite"
+    for key, value in stats.items():
+        print(f"{key} = {value}")
+elif class_choice == "lich":
+    stats["health"] = 40
+    stats["defense"] = 10
+    stats["attack"] = "D20 + 2"
+    stats["damage"] = "D8"
+    stats["attack one"] = "draining touch"
+    stats["attack two"] = "death bolt"
+    for key, value in stats.items():
+        print(f"{key} = {value}")
+elif class_choice == "demon":
+    stats["health"] = 20
+    stats["defense"] = 15
+    stats["attack"] = "D20"
+    stats["damage"] = "D10"
+    stats["attack one"] = "burning claw"
+    stats["attack two"] = "torment"
+    for key, value in stats.items():
+        print(f"{key} = {value}")
+else:
+    print("Incorrect input try again")
+def player_turn():
+    attack_choice = input(f"type 1 for attack {stats['attack one']} or type 2 for attack {stats['attack two']} type 2?").strip().lower()
+    if attack_choice == "1":
+        print(f"You used {stats['attack one']}")
+        if class_choice == "bunny":
+            attack_roll = random.randint(1,20) + 2
+            print(f"you rolled a {attack_roll}")
+            time.sleep(3)
+            if attack_roll >= monster["defense"]:
+                damage_roll = random.randint(1,10)
+                print(f"You hit the diamond ant for {damage_roll} damage!")
+                time.sleep(3)
+                monster["health"] -= damage_roll
+                damage_roll = (random.randint(1,10)) * 0.5
+                print(f"You hit the diamond ant for an additional {damage_roll} damage with your multikick!")
+                monster["health"] -= damage_roll
+                time.sleep(3)
+                if monster["health"] <= 0:
+                    print("You have defeated the diamond ant!")
+                    monster_life = "dead"
+            else:
+                print("You missed!")
+        elif class_choice == "lich":
+            attack_roll = random.randint(1,20) + 2
+            print(f"you rolled a {attack_roll}")
+            time.sleep(3)
+            if attack_roll >= monster["defense"]:
+                damage_roll = random.randint(1,8)
+                print(f"You hit the diamond ant for {damage_roll} damage!")
+                time.sleep(3)
+                monster["health"] -= damage_roll
+                stats["health"] += damage_roll
+                print(f"You drained {damage_roll} health from the diamond ant!")
+                time.sleep(3)
+                if monster["health"] <= 0:
+                    print("You have defeated the diamond ant!")
+                    monster_life = "dead"
+            else:
+                print("You missed!")
+        elif class_choice == "demon":
+            attack_roll = random.randint(1,20)
+            print(f"you rolled a {attack_roll}")
+            time.sleep(3)
+            if attack_roll >= monster["defense"]:
+                damage_roll = random.randint(1,10)
+                print(f"You hit the diamond ant for {damage_roll} damage!")
+                time.sleep(3)
+                monster["health"] -= damage_roll
+                if monster["health"] <= 0:
+                    print("You have defeated the diamond ant!")
+                    monster_life = "dead"
+            else:
+                print("You missed!")
+    if attack_choice == "2":
+        print(f"You used {stats['attack one']}")
+        if class_choice == "bunny":
+            attack_roll = random.randint(1,20) + 2
+            print(f"you rolled a {attack_roll}")
+            time.sleep(3)
+            if attack_roll >= monster["defense"]:
+                damage_roll = (random.randint(1,10))*1.2
+                print(f"You hit the diamond ant for {damage_roll} damage!")
+                monster["health"] -= damage_roll
+                time.sleep(3)
+                damage_roll = (random.randint(1,10)) * 0.3
+                print(f"You hit the diamond ant for an additional {damage_roll} damage with your multikick!")
+                time.sleep(3)
+                monster["health"] -= damage_roll
+                if monster["health"] <= 0:
+                    print("You have defeated the diamond ant!")
+                    monster_life = "dead"
+            else:
+                print("You missed!")
+        elif class_choice == "lich":
+            attack_roll = random.randint(1,20) + 2
+            print(f"you rolled a {attack_roll}")
+            time.sleep(3)
+            if attack_roll >= monster["defense"]:
+                damage_roll = (random.randint(1,8)) * 1.1
+                print(f"You hit the diamond ant for {damage_roll} damage!")
+                time.sleep(3)
+                monster["health"] -= damage_roll
+                if monster["health"] <= 0:
+                    print("You have defeated the diamond ant!")
+                    monster_life = "dead"
+            else:
+                print("You missed!")
+        elif class_choice == "demon":
+            attack_roll = random.randint(1,20)
+            print(f"you rolled a {attack_roll}")
+            time.sleep(3)
+            if attack_roll >= monster["defense"]:
+                damage_roll = (random.randint(1,10)) * 1.3
+                print(f"You hit the diamond ant for {damage_roll} damage!")
+                time.sleep(3)
+                monster["health"] -= damage_roll
+                if monster["health"] <= 0:
+                    print("You have defeated the diamond ant!")
+                    monster_life = "dead"
+            else:
+                print("You missed!")
+def monster_turn():
+    print("The diamond ant is attacking!")
+    attack = random.randint(1,2)
+    if attack == 1:
+        print("The diamond and sprays acid at you!")
+        attack_roll = random.randint(1,20) + 2
+        print(f"The diamond ant rolled a {attack_roll}")
+        time.sleep(3)
+        if attack_roll >= stats["defense"]:
+            damage_roll = random.randint(1,8) + 3
+            print(f"The diamond ant hit you for {damage_roll} damage!")
+            time.sleep(3)
+            stats["health"] -= damage_roll
+            print(f"Your health is now {stats['health']}")
+            time.sleep(3)
+            if stats["health"] <= 0:
+                print("You have been defeated by the diamond ant!")
+                player = "dead"
         else:
-            print("The Diamond Ant attacks")
-    class_choice = input("Do you want to be a sacraficial warrior, a blur knife, or a demon blood").strip()
-    health = 0
-    defense = 0
-    attack = 0
-    damage = 0
-    if class_choice == "sacraficial warrior":
-        print("Here are your stats.")
-        health = 40
-        defense = 10
-        attack = f"D20 + {health/20}"
-        damage = f"D6 + {health/40}"
-    elif class_choice == "blur knife":
-        print("Here are your stats.")
-        health = 20
-        defense = 15
-        attack = f"D20 + 1"
-        damage = f"D8 + 1"
-    elif class_choice == "demon blood":
-        print("Here are your stats.")
-        health = 10
-        defense = 17
-        attack = f"D20"
-        damage = f"D10 + 2"
+            print("The diamond ant missed!")
+    if attack == 2:
+        print("The diamond ant bites you!")
+        time.sleep(3)
+        attack_roll = random.randint(1,20) + 2
+        print(f"The diamond ant rolled a {attack_roll}")
+        time.sleep(3)
+        if attack_roll >= stats["defense"]:
+            damage_roll = random.randint(1,8) + 3
+            print(f"The diamond ant hit you for {damage_roll} damage!")
+            time.sleep(3)
+            stats["health"] -= damage_roll
+            print(f"Your health is now {stats['health']}")
+            if stats["health"] <= 0:
+                print("You have been defeated by the diamond ant!")
+                player = "dead"
+        else:
+            print("The diamond ant missed!")
+order = random.randint(1,2)
+while player == "alive" and monster_life == "alive":
+    print("You are being attacked by a diamond ant!")
+    if order == 1:
+        player_turn()
+        time.sleep(3)
+        if monster_life == "alive":
+            monster_turn()
     else:
-        print("Incorrect input try again")
+        monster_turn()
+        time.sleep(3)
+        if player == "alive":
+            player_turn()
