af6803a9a9ac37d5745521030c65d65059c857f3
guy-glitch <warrenjgib@gmail.com>
2025-11-04 09:53:10 -0700

    maze generator

--- Practice/maze_generator.py
+++ Practice/maze_generator.py
@@ -2,13 +2,14 @@
 #import turtle and random as t and r
 import turtle as t
 import random as r
-#list row_grid
-row_grid = 
-#list col_grid
-col_grid = 
-#def of maze_design
-def maze_design():
-
+#set up the screen
+screen = t.Screen()
+screen.setup(800,800)
+#list row_grid detirmines if there is a wall there
+row_grid = [
+    [r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)]]
+#list col_grid that detirmenes if there is a wall there
+col_grid = [[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)],[r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1),r.randint(0,1)]]
 #Def is_solvable
 def is_solvable(row_grid, col_grid):
     #set the size as one less then the length of row grid
@@ -30,11 +31,11 @@ def is_solvable(row_grid, col_grid):
             #continue to next iteration of the loop
             continue
         #Add x, y to visited
-        visited.add(x, y)
+        visited.add((x,y))
         #check if x is less then size and if y and x increased by one is still in the grid
         if x < size - 1 and col_grid[y][x+1] == 0:
             #add x plus one to the stack 
-            stack.append((x+1, y))
+            stack.append((x+1,y))
         #check if x is less then size and if y and x increased by one is still in the grid
         if y < size - 1 and row_grid[y+1][x] == 0:
             #add x plus one to the stack 
@@ -49,3 +50,26 @@ def is_solvable(row_grid, col_grid):
             stack.append((x, y-1))
 #if not solvable will return false
     return False
+#check if is solvable is true
+if is_solvable(row_grid, col_grid)==True:
+    t.teleport(-300,-300)
+    t.color("black")
+    t.left(90)
+    t.forward(600)
+    t.right(90)
+    t.forward(500)
+    t.teleport(300,300)
+    t.right(90)
+    t.forward(600)
+    t.right(90)
+    t.forward(500)
+    for num in row_grid:
+        for num1 in row_grid[num]:
+            t.teleport(200+num1*100,num*100+100)
+            if row_grid[num][num1]==1:
+                t.forward(200)
+                continue
+            else:
+                continue
+
+t.done()
