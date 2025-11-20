8d3168460ade03a0bf1b6b338e84a22fa6555b58
guy-glitch <warrenjgibson@gmial.com>
2025-11-20 09:33:45 -0700

    bored

---
+++ for_fun.py/crash.py
@@ -0,0 +1,12 @@
+#set number to 2
+number = 2
+#while true
+while True:
+    #add one to numbr
+    number += 1
+    #print number
+    print(number)
+    #multiply number by 2
+    number *= 2
+    #print number
+    print(number)
