652171f784c5447bfda2eb5e38433988ff6a05f8
guy <warrenjgib@gmail.com>
2025-11-21 20:39:16 +0000

    factorial calculator based of given pseusdocode finished

--- Practice/factorial_calculator.py
+++ Practice/factorial_calculator.py
@@ -1,4 +1,5 @@
 #WG 1st factorial Calculator Zane pseudocode
+#double hash comments are edits to original pseudocode
 #define fact(num)
 def fact(num):
     #total = 1, or = num #set total to 1
@@ -16,12 +17,16 @@ while True:
     #or#switched to num = ask user to give number.strip()#assign it to a variable up here#give instructions please give me a whole non-zero number that you want the factorial of
     num = input("Give me a whole non-zero number that you want the factorial of.").strip()
     #assign that to a vaiable if variable is letters restart
-    if num.isdigit:
-        #check to make sure it is a number 
-        if num.is
-        #round(num,0)
-        #fact(user_num)
-        #print(f"new:{total}, original:(or)")
+    if num.isdigit():
+        #convert variable to float
+        num = float(num)
+        ##check if variable is a whole non-zero number
+        if num.is_integer() and num >= 0:
+            ##removed the rounding of num undeeded
+            #fact(user_num#just num)
+            total = fact(int(num))
+            #print(f"new:{total}, original:(or#switched to num)")\
+            print(f"Factorial is: {total}, the original number was: {int(num)}")
         else:
             continue
     else:
