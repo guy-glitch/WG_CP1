298f5ebf3bb251814f1185b82e5e6b9f84c9dbf0
guy-glitch <warrenjgibson@gmial.com>
2025-11-25 09:52:56 -0700

    text adventure game pseudocode start

---
+++ final/text_adventure_game.py
@@ -0,0 +1,34 @@
+#WG 1st Text Based Adventure Game
+#Create Remy's stat dictionary with three stats Star rating: 4, Cooking Sense: 5%, Control: 75%
+
+#Last name dictionary of critic last names Tommato, Zucchhini, Mozzeralla, Baslil, Sausasge, Peperoncini, Swoup
+
+#First name dictionary of critic first names Circel, Zgiazg, Sritpe, Retow, Tralun
+
+#variable trained = no
+
+#variable cooking_practice = no
+
+ #create variable cooking pot = uncollected
+
+#Create the function for the kitchen with the cooking pot as a variable
+
+    #variable entered = no
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+        #if cooking pot = uncollected
+
+            #pot_collection = ask if they want to collect the pot
+
+            #if pot_collection is yes
+
+                #increase the star rating by 0.1 stars
+
+                #cooking pot = collected
