4c6978150bf2df71d8bc307dc2b8d76f57f25d4d
guy-glitch <warrenjgibson@gmial.com>
2025-12-01 09:51:45 -0700

    rattitoulle adventure game pseudocode start

--- final/text_adventure_game.py
+++ final/text_adventure_game.py
@@ -1,9 +1,13 @@
 #WG 1st Text Based Adventure Game
+#import random as r
+
 #Create Remy's stat dictionary with three stats Star rating: 4, Cooking Sense: 5%, Control: 75%
 
-#Last name dictionary of critic last names Tommato, Zucchhini, Mozzeralla, Baslil, Sausasge, Peperoncini, Swoup
+#Last name dictionary of critic last names, correlated with the ingredient that name enjoys Tommato, Zucchhini, Mozzeralla, Baslil, Sausasge, Peperoncini, Swoup
 
-#First name dictionary of critic first names Circel, Zgiazg, Sritpe, Retow, Tralun
+#First name dictionary of critic first names correlated with the presentation wanted Circel, Zgiazg, Sritpe, Retow
+
+#create dictionary for the head critic first name = Carh, last name = LePineapp
 
 #variable trained = no
 
@@ -11,7 +15,62 @@
 
  #create variable cooking pot = uncollected
 
-#Create the function for the kitchen with the cooking pot as a variable
+#create variable practice meal = unmade
+
+#create a list of the ingredients that they can choose from at base level.
+
+#create a list of the ingredients that are in the pantry
+
+#create a function for moving around
+
+    #allow them to choose what room they want to go into
+
+    #check what room they want to go into and 
+
+#create the function for cooking
+    
+    #display the cooking instructions
+
+    #display the order name based on function critic name
+
+    #let them choose if they want to cook for the critic
+
+    #if they choose to cook for the critic:
+
+        #ask them if they want to go to the pantry or just cook with what they have on hand
+
+        #if they want to go to the pantry
+            
+            #display the ingredients in the pantry      
+
+            #let them choose what ingredients they want to take and use, with a maximum of five
+
+
+        #else
+
+            #display the basic ingredeints list
+
+            #let them choose what ingredients they want to use, with a maximum of six
+
+        #let them choose the presentation they want to use form a set list
+
+        #check if the ingredients correlated with the name is in the meal
+
+            #increase star rating by 0.1 and display the orginal and the new
+
+        #check if the presentation correlated with the first name is used
+
+            #increase star rating by 0.1 and display the orginal and the new
+
+    #else if they choose not to cook for the critic
+
+        #decrease star rating by .5
+
+    #else:
+
+        #rerun the function
+
+#Create the function for the dining room with argument delegated
 
     #variable entered = no
 
@@ -23,12 +82,117 @@
 
     #while entered == yes
 
-        #if cooking pot = uncollected
+#Create the function for the office
 
-            #pot_collection = ask if they want to collect the pot
+    #variable entered = no
 
-            #if pot_collection is yes
+    #if variable entered == no
 
-                #increase the star rating by 0.1 stars
+        #describe the room
 
-                #cooking pot = collected
+        #Set enter to yes
+
+    #while entered == yes
+
+#Create the function for the pantry
+
+    #variable entered = no
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+#Create the function for the street
+
+    #variable entered = no
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+#Create the function for the private dining room
+
+    #variable entered = no
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+#Create the function for the rat's nest
+
+    #variable entered = no
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+#Create the function for the entery hall
+
+    #variable entered = no
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+#create the function that generates the critics name 
+
+    #critic name = []
+
+    #get a random value from critics first name dictionary
+
+    #get a random value from the critic last name dictionary
+
+    #get the key that corresponds with the value of the first name and add to the list critic name
+
+    #get the key that corresponds with the value of the first name and add to the list critic name
+
+    #return critic name in a well formatted list
+
+#give starting intro
+
+#while loop that is infinite
+
+    #ask them if they want to go to work with Alfredo Linguini
+
+    #if they don't wan to go to work 
+
+        #allow them to choose wether or not they want to return to the rat's nest or wander the streets allown
+
+        #if they return to rat's nest
+
+            #call rat's nest function
+
+        #else if they want to be alone
+
+            #describe them running into a mouse trap and dying
+
+            #break
+        #else
+
+            #continue
+
+    #if else they do want to go to work with him
+
+        #send them to work with him
+
+#describe them dying
