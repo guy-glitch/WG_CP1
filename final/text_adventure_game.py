924d67ffb459c01dc89b4d50530bd3ffe6d92f3b
guy-glitch <warrenjgibson@gmial.com>
2025-12-03 09:52:19 -0700

    text adventure game code started

--- final/text_adventure_game.py
+++ final/text_adventure_game.py
@@ -1,59 +1,72 @@
 #WG 1st Text Based Adventure Game
 #import random as r
-
+import random as r
 #Create Remy's stat dictionary with three stats Star rating: 4, Cooking Sense: 5%, Control: 75%
-
+remy = {"Star rating": 4, "Cooking sense": 5, "Control": 75}
 #Last name dictionary of critic last names, correlated with the ingredient that name enjoys Tommato, Zucchhini, Mozzeralla, Baslil, Sausasge, Peperoncini, Swoup
-
+critic_last ={"Tommato": "Tomato", "Zuchhini":"Zuchini", "Mozzeralla":"Mozzarella", "Baslil":"Basil", "Sausasge":"Sausage", "Peperocnini":"Peperoncini", "Galcir":"Garlic"}
 #First name dictionary of critic first names correlated with the presentation wanted Circel, Zgiazg, Sritpe, Retow
+critic_first = {"Circel":"Circle", "Zgiazg":"Zigzag", "Sritpe":"Stripe", "Retow":"Tower"}
+#list of possible formats for the meal
 
 #create dictionary for the head critic first name = Carh, last name = LePineapp
-
+head_critic = {"first name":"Carh", "last name":"LePineapp"}
 #variable trained = no
-
+trained = False
 #variable cooking_practice = no
-
+cooking_practice = False
  #create variable cooking pot = uncollected
-
+cooking_pot = "uncollected"
 #create variable practice meal = unmade
-
+practice_meal = "unmade"
 #create a list of the ingredients that they can choose from at base level.
-
+base_ingredients = ["Tomato", "Zuchini", "Mozzarella", "Sausage", "Peperoncini", "Basil", "Garlic"]
 #create a list of the ingredients that are in the pantry
-
-
+pantry_ingredients = ["Pineapple","Cheese","Mozzarella","Sausage","Peperoncini","Basil","Tomato","Zuchini","Garlic","Butter", "Olive oil", "Cream","Nutmeg","Cardomom"]
+#create location variable
+location = "aparment"
+#list of possible presenattion
+presentation = []
+#ingredients used in the meal
+ingredients = []
 #create a function for moving around
-
+def moving():
     #allow them to choose what room they want to go into
-
-    #check what room they want to go into and
-
+    room = input("Do you want to go to the kitchen, the dining room, the office, the pantry, the private dining room, the rat's next, the street, the entery hall, or the apartment")
+    #check what room they want to go into and move them there
+    return room
 #create the function for cooking
-   
+def cooking(critic_name):
     #display the cooking instructions
-
+    print("To cook select the ingredients you want to add to your meal, then select the format")
     #display the order name based on function critic name
-
+    print(critic_name                                  )
     #let them choose if they want to cook for the critic
-
+    cooking = input("Do you want to cook for the critic")
     #if they choose to cook for the critic:
-
+    if cooking == "yes":
+        ingredients = []
+        meal = []
         #ask them if they want to go to the pantry or just cook with what they have on hand
-
+        ingredient_location = input("Do you want to go to the pantry or use the ingredeints you have")
         #if they want to go to the pantry
-           
+        if ingredient_location == "pantry":
             #display the ingredients in the pantry      
-
+            print(pantry_ingredients)
             #let them choose what ingredients they want to take and use, with a maximum of five
-
+            for i in range(5):
+                ingredient = input("What ingredeints do you want to use?").strip()
+                ingredients.append(ingredient)
         #else
-
+        else:
             #display the basic ingredients list
-
+            print(base_ingredients)
             #let them choose what ingredients they want to use, with a maximum of six
-
+            for i in range(6):
+                ingredient = input("What ingredeint do you want to use?").strip()
+                ingredients.append(ingredient)
         #let them choose the presentation they want to use form a set list
-
+        print(p)
         #using there control stat as a basis give them a certain percent chance of getting caught
 
         #check if the ingredients correlated with the name is in the meal
@@ -72,6 +85,25 @@
 
         #rerun the function
 
+#Create the function for the office
+
+
+    #variable entered = no
+
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+        #if they came in here to be congratulated
+
+            #give them a congratulated message for raising the star rating
+
+        #if they came because they dropped the star rating give them an angry message and send them out to be a server
 
 #Create the function for the dining room with argument delegated
 
@@ -139,6 +171,25 @@
 
         #if they came because they dropped the star rating give them an angry message and send them out to be a server
 
+#Create the function for the street
+
+    #variable entered = no
+
+    #if variable entered == no
+
+        #describe the room
+
+        #Set enter to yes
+
+    #while entered == yes
+
+        #if they came in here to be congratulated
+
+
+            #give them a congratulated message for raising the star rating
+
+
+        #if they came because they dropped the star rating give them an angry message and send them out to be a server
 
 #Create the function for the pantry
 
@@ -168,6 +219,7 @@
 
 
         #return the ingredients that they chose
+
 #Create the function for the street
 
 
@@ -332,6 +384,3 @@
 
 
 #describe them dying
-
-
-
